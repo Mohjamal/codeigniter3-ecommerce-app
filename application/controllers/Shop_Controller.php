7f0cc2fef85ac57d54e1011f9b2086865ba3c970
MohJamal <mohjamalalkhatib96@gmail.com>
2025-08-28 14:34:33 +0300

    Getting Started of client side of the project => make shop controller and view + set default route to Shop_Controller + insert shop theme

---
+++ application/controllers/Shop_Controller.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Shop_Controller extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('shop/index');
+    }
+}
